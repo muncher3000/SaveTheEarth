6c7392cedf1a97602f961daeceaeb090da5cf555
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-07-26 11:49:54 +0800

    Initial Commit

---
+++ saveTheEarth/saveTheEarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  saveTheEarthApp.swift
+//  saveTheEarth
+//
+//  Created by Sharlene Tan Qin Ying on 26/7/25.
+//
+
+import SwiftUI
+
+@main
+struct saveTheEarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
