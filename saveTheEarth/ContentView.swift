a4b39c1ce6a138f6825cbd41d810ff6f579db082
Divya <divya_dharshini_krishnamoorthy@students.edu.sg>
2025-08-16 10:03:50 +0800

    I tried to work on the first page, the earth image isn't showing up help but the button is

--- saveTheEarth/ContentView.swift
+++ saveTheEarth/ContentView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var isEarthShow = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Save The Earth")
+            if isEarthShow == true{
+                Image("earth")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            if isEarthShow == false {}
+            Button {
+                if isEarthShow == true {
+                    isEarthShow = false
+                }
+                
+            } label: {
+                Text("Save the earth")
+                    .font(.largeTitle)
+                    .monospaced()
+                    .bold()
+                    .background(.green)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+                    
+            }
         }
         .padding()
     }
