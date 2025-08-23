7400560f5f473b7e011b0133b82ce4f8688759d5
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-08-23 09:15:58 +0800

    HIII

---
+++ saveTheEarth/CheckpointView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckpointView.swift
+//  saveTheEarth
+//
+//  Created by Sharlene Tan Qin Ying on 2/8/25.
+//
+
+import SwiftUI
+
+struct CheckpointView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckpointView()
+}
