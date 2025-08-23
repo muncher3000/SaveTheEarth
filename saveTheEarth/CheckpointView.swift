9f44b328ad5ccabbe6cbab2c8ffa28bbdfb32faa
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-08-23 09:16:30 +0800

    changed some stuff

--- saveTheEarth/CheckpointView.swift
+++ saveTheEarth/CheckpointView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CheckpointView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+            .cornerRadius(65)
     }
 }
 
--- saveTheEarth/ContentView.swift
+++ saveTheEarth/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let green = Color(hue: 121, saturation: 37, brightness: 87, opacity: 1)
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Save The Earth")
+                .font(.system(size: 200))
+            Text("Save The World")
+                .fontDesign(.monospaced)
+                .font(.system(size: 40, weight: .bold))
+            NavigationLink {
+                CheckpointView()
+            } label: {
+                Label("", systemImage: "restart")
+                    .scaleEffect(x: -1, y: -1, anchor: .center)
+                    .bold()
+                    .font(.system(size : 50))
+                    .foregroundColor(green)
+                    .padding(10)
+                    
+            }
+            
+            }
         }
-        .padding()
-    }
 }
 
 #Preview {
