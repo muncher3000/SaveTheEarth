407f7e65aaa0db7f800c42ed6f9f7bf9502b8065
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-08-23 10:54:32 +0800

    HI

---
+++ saveTheEarth/AcidFoamView.swift
@@ -0,0 +1,18 @@
+//
+//  AcidFoamView.swift
+//  saveTheEarth
+//
+//  Created by Sharlene Tan Qin Ying on 23/8/25.
+//
+
+import SwiftUI
+
+struct AcidFoamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AcidFoamView()
+}
