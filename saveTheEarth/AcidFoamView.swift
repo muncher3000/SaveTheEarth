f149ffce57c79934d96b1c51fc2a145db4857fa9
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-09-07 22:19:53 +0800

    THE DETECTION FINALLY WORKS EQOFIEFHG (not touching that agn!)

--- saveTheEarth/AcidFoamView.swift
+++ saveTheEarth/AcidFoamView.swift
@@ -13,9 +13,10 @@ struct AcidFoamView: View {
     @State private var shown: Bool = true
     @State private var umbrellaShown: Bool = false
     @State private var points : Int = 0
+    @State private var gameWon: Bool = false
+    
     var body: some View {
         ZStack{
-          
             
             Image("CityBackground")
                 .resizable()
@@ -23,7 +24,7 @@ struct AcidFoamView: View {
                 .frame(width:500)
                 .padding(.top, 450)
           
-            SpriteView(scene: RainFall(), options: [.allowsTransparency])
+            SpriteView(scene: createRainScene(), options: [.allowsTransparency])
             
             if umbrellaShown == true {
                 Image("Umbrella")
@@ -31,7 +32,6 @@ struct AcidFoamView: View {
                     .scaledToFit()
                     .frame(width:100)
                     .offset(offset)
-                    
                     .gesture(
                         DragGesture()
                             .onChanged { value in
@@ -39,16 +39,23 @@ struct AcidFoamView: View {
                             }
                     )
                 
-                Image("Rai")
-                                    
                 Button{
                     resetUmbrella()
                 }label: {
                     Text("Reset umbrella position")
                 }
                 .padding(.top, 740)
+                
+                
+                Text("Points: \(points)")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                    .background(Color.black.opacity(0.6))
+                    .cornerRadius(10)
+                    .padding(.top, 650)
+                    
             }
-
             
             if shown == true {
                 ZStack{
@@ -58,9 +65,7 @@ struct AcidFoamView: View {
                         .opacity(1)
                         .cornerRadius(20)
                         .overlay(GeometryReader{_ in
-                            
-                              SpriteView(scene: RainFallLanding(), options: [.allowsTransparency])
-
+                            SpriteView(scene: RainFallLanding(), options: [.allowsTransparency])
                         })
                     VStack{
                         Button{
@@ -78,27 +83,59 @@ struct AcidFoamView: View {
                             .bold()
                         Text("Acid rain will fall, use the umbrella to block it!")
                     }
-                    
-                    
                 }
                 .padding(.bottom, 100)
             }
-                
-       
             
-
+            
+            if gameWon {
+                
+                ZStack {
+                    Rectangle()
+                        .frame(width: 350, height: 200)
+                        .foregroundStyle(.green)
+                        .opacity(0.9)
+                        .cornerRadius(20)
+                    
+                    VStack {
+                        Text("Congratulations!")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(.white)
+                        
+                        Text("yeppp")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .padding(.top, 5)
+                        
+                        Text("Final Score: \(points)")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        
+                        
+                    }
+                }
+            }
         }
         .navigationBarBackButtonHidden(true)
         .ignoresSafeArea()
-        
-    }
-    func resetUmbrella() {
-        offset = .zero
     }
     
-    func raining() {
-        
-      
+    func createRainScene() -> RainFall {
+        let scene = RainFall()
+        scene.umbrellaPosition = { self.offset }
+        scene.onPointsChange = { newPoints in
+            self.points = newPoints
+            if self.points >= 15 {
+                self.gameWon = true
+            }
+        }
+        return scene
+    }
+    
+    func resetUmbrella() {
+        offset = .zero
     }
 }
 
@@ -106,23 +143,112 @@ struct AcidFoamView: View {
     AcidFoamView()
 }
 
-
 class RainFall : SKScene {
-    override  func sceneDidLoad() {
+    var umbrellaPosition: (() -> CGSize)?
+    var onPointsChange: ((Int) -> Void)?
+    var umbrellaNode: SKNode!
+    var currentPoints: Int = 0
+    
+    override func sceneDidLoad() {
         size = UIScreen.main.bounds.size
         scaleMode = .resizeFill
-        
         anchorPoint = CGPoint(x: 0.5 , y: 1)
         backgroundColor = .clear
+        
         let node = SKEmitterNode(fileNamed: "RainFall.sks")!
         addChild(node)
-        
         node.particlePositionRange.dx = UIScreen.main.bounds.width
+        
+        
+        umbrellaNode = SKNode()
+        umbrellaNode.position = CGPoint(x: 0, y: -UIScreen.main.bounds.height + 200)
+        
+        
+        let umbrellaSize = CGSize(width: 80, height: 20)
+        umbrellaNode.physicsBody = SKPhysicsBody(rectangleOf: umbrellaSize)
+        umbrellaNode.physicsBody?.isDynamic = false
+        umbrellaNode.physicsBody?.categoryBitMask = 2
+        umbrellaNode.physicsBody?.contactTestBitMask = 1
+        umbrellaNode.physicsBody?.collisionBitMask = 0
+        
+        addChild(umbrellaNode)
+        
+       
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector(dx: 0, dy: -1)
+        BiggRainDroplets()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+        
+        if let getPosition = umbrellaPosition {
+            let offset = getPosition()
+            umbrellaNode.position = CGPoint(
+                x: offset.width,
+                y: -UIScreen.main.bounds.height + 200 + offset.height
+            )
+        }
+        
+        enumerateChildNodes(withName: "raindrop") { node, _ in
+            if node.position.y < -self.size.height {
+                node.removeFromParent()
+            }
+        }
+    }
+    
+    func BiggRainDroplets() {
+        let spawnAction = SKAction.run {
+            self.spawnRaindrop()
+        }
+        let waitAction = SKAction.wait(forDuration: 1.0)
+        let sequenceAction = SKAction.sequence([spawnAction, waitAction])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        
+        run(repeatAction)
+    }
+    
+    func spawnRaindrop() {
+       
+        let raindrop = SKSpriteNode(imageNamed: "Rain")
+        raindrop.name = "raindrop"
+        raindrop.size = CGSize(width: 30, height: 30)
+        
+
+        let randomX = CGFloat.random(in: -size.width/2...size.width/2)
+        raindrop.position = CGPoint(x: randomX, y: size.height/2)
+        
+     
+        raindrop.physicsBody = SKPhysicsBody(rectangleOf: raindrop.size)
+        raindrop.physicsBody?.categoryBitMask = 1
+        raindrop.physicsBody?.contactTestBitMask = 2
+        raindrop.physicsBody?.collisionBitMask = 0
+        raindrop.physicsBody?.affectedByGravity = true
+        
+        addChild(raindrop)
+    }
+}
+
+extension RainFall: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        
+        if contactMask == 3 {
+            currentPoints += 1
+            onPointsChange?(currentPoints)
+            
+      
+            if contact.bodyA.categoryBitMask == 1 {
+                contact.bodyA.node?.removeFromParent()
+            } else {
+                contact.bodyB.node?.removeFromParent()
+            }
+        }
     }
 }
 
 class RainFallLanding : SKScene {
-    override  func sceneDidLoad() {
+    override func sceneDidLoad() {
         size = UIScreen.main.bounds.size
         scaleMode = .resizeFill
         
@@ -135,4 +261,4 @@ class RainFallLanding : SKScene {
     }
 }
 
-// https://www.youtube.com/watch?v=_Ssae8NLHUs rainfall landing on umbrella 4 min 30 s
+// https://www.youtube.com/watch?v=_Ssae8NLHUs RainFall + Collision tutorial
