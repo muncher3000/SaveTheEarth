5b55745da70fd85d510000bea9d76e55c67aca73
Sharlene Tan Qin Ying <sharlene_tan_qin_ying@s2024.ssts.edu.sg>
2025-09-08 14:42:23 +0800

    yep

--- saveTheEarth/PianoTileGameView.swift
+++ saveTheEarth/PianoTileGameView.swift
@@ -11,16 +11,40 @@ import AVFoundation
 struct PianoTileGameView: View {
     var sound = true
     @State var audioPlayer: AVAudioPlayer!
+    @State var gameStart = false
+    @State var instructions = true
 
     var body: some View {
-        ZStack {
-                    Text("OMG SONGPLAYS YAAYYYAYAYAY")
+        if instructions == true{
+            ZStack {
+                Rectangle()
+                    .cornerRadius(20)
+                    .frame(width: 350, height: 150)
+                    .foregroundStyle(.gray)
+                    .opacity(0.3)
+                VStack{
+                    Text("Instructions:")
+                        .bold()
+                        .font(.largeTitle)
+                    Text("Tap the fallign foam when it reaches the colourful circles")
+                    Button{
+                        gameStart = true
+                    }label:{
+                        Text("Start")
+                    }
                 }
-                .onAppear(perform: {
-                    playSounds("song.mp3")
-                })
-        .navigationBarBackButtonHidden(true)
+            }
+            .onAppear(perform: {
+                playSounds("song.mp3")
+            })
+            .navigationBarBackButtonHidden(true)
+        }
+        if gameStart == true{
+            
+        }
     }
+    
+   
     func playSounds(_ soundFileName : String) {
             guard let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: nil) else {
                 fatalError("Unable to find \(soundFileName) in bundle")
